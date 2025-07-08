eac373bdeb65aeec2f9b8e1d91d255e4c24b7de6
Aluno <ythalo.abreu@escola.pr.gov.br>
2025-07-08 12:09:23 -0300

    Implementação upadate

--- src/main/java/com/mypet2/pet/repository/ResponsavelRepository.java
+++ src/main/java/com/mypet2/pet/repository/ResponsavelRepository.java
@@ -1,9 +1,14 @@
 package com.mypet2.pet.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.mypet2.pet.modelo.Responsavel;
 
 public interface ResponsavelRepository extends JpaRepository<Responsavel, Long> {
     
+    @Query (value =  "select * from responsavel where nome like ?", nativeQuery = true)
+    public Responsavel findByNome(String nome);
+
+
 }
